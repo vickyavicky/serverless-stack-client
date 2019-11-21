483860359fc185a0d212359d226031c74b944c95
Vicky Avicky <vickyavicky23@gmail.com>
2019-11-21 19:22:32 +0300

    Setting up React app

---
+++ src/containers/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+    return (
+        <div className="NotFound">
+            <h3>Aww, Snap! You found our junior developers page!</h3>
+            <p>Despite sleeping on the couch all day, our junior developers have time to do some little coding</p>
+        </div>
+    );
+}
